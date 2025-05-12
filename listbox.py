f39dcc954053b8a867bc8935dc0fdd7b33611215
Berthus001 <147304013+Berthus001@users.noreply.github.com>
2025-05-12 19:07:24 +0800

    Update listbox.py

--- listbox.py
+++ listbox.py
@@ -271,7 +271,7 @@ def print_resume():
         })
 
         x = 100
-        y = 100
+        y = 50
         line_width = 2800
 
         def draw_line():
@@ -279,7 +279,7 @@ def print_resume():
             y += 20
             pdc.MoveTo((x, y))
             pdc.LineTo((line_width, y))
-            y += 150
+            y += 20
 
         # Name
         y += 50
@@ -311,7 +311,7 @@ def print_resume():
         for line in skills.splitlines():
             pdc.TextOut(x, y, line)
             y += 130
-        y += 50
+        y += 150
         draw_line()
 
         # Education
@@ -323,7 +323,7 @@ def print_resume():
         for line in education.splitlines():
             pdc.TextOut(x, y, line)
             y += 130
-        y += 50
+        y += 150
         draw_line()
 
         # Working Experience
@@ -335,7 +335,7 @@ def print_resume():
         for line in working_experience.splitlines():
             pdc.TextOut(x, y, line)
             y += 130
-        y += 50
+        y += 150
         draw_line()
 
         # Professional Summary
@@ -346,7 +346,7 @@ def print_resume():
         pdc.SelectObject(content_font)
         for line in summary.splitlines():
             pdc.TextOut(x, y, line)
-            y += 100 
+            y += 100
 
         pdc.EndPage()
         pdc.EndDoc()
